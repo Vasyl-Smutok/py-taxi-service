51c8797b99336d70a30d6f0685b23585400cf6dd
Vasyl Smutok <23.smutok@gmail.com>
2022-07-28 08:46:42 +0300

    disable debug

--- taxi_service/settings.py
+++ taxi_service/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', "django-insecure-8ovil3xu6=eaoqd#-#&ricv159p0pypoh5_lgm*)-dfcjqe=yc")
 
 # SECURITY WARNING: don"t run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DJANGO_DEBUG", "") != False
 
 ALLOWED_HOSTS = ["127.0.0.1", "taxi-service-smutok.herokuapp.com"]
 
